a074e28504f5f437b1c253f48c1c91f8f9ad3eb6
32844583 <youto201255@gmail.com>
2025-01-30 22:10:51 +0800

    local to server version

--- public/networking.js
+++ public/networking.js
@@ -1,13 +1,14 @@
 // networking.js (Socket.IO 版本)
 
 // 假如你的伺服器位於同網域/同網址，就可以直接空白 io()
-const socket = io("http://localhost:3000");
+// const socket = io("http://localhost:3000");
+
+// 如果你要指定網址，可以使用：
+const socket = io("https://my-game-aapb.onrender.com");
+// 但實際上 socket.io 預設會用 https:// 或 wss:// 動態連線
 socket.on("connect", () => {
     console.log("本機連線成功，socket.id =", socket.id);
 });
-// 如果你要指定網址，可以使用：
-// const socket = io("wss://my-game-aapb.onrender.com");
-// 但實際上 socket.io 預設會用 https:// 或 wss:// 動態連線
 
 document.getElementById("player1-ready").addEventListener("click", () => {
   socket.emit("ready", { player: 1 });
