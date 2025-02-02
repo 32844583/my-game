8e4a097be712e233876fbc1dd76aef9ffeaf2df7
32844583 <youto201255@gmail.com>
2025-02-02 15:28:21 +0800

    Deploy

--- public/NetworkManager.js
+++ public/NetworkManager.js
@@ -1,12 +1,12 @@
 // NetworkManager.js
 // 建立 socket 連線，未來所有模組都從此取得 socket 實例
-const socket = io("http://localhost:3000");
+// const socket = io("http://localhost:3000");
 
 // 伺服器端上線後使用
-// const socket = io("https://my-game-aapb.onrender.com", {
-//   withCredentials: true,
-//   transports: ['websocket', 'polling']
-// });
+const socket = io("https://my-game-aapb.onrender.com", {
+  withCredentials: true,
+  transports: ['websocket', 'polling']
+});
 
 socket.on("connect", () => {
   console.log("Connected to server");
